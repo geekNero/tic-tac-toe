6a943f289fd1baac7b89c69f833ccd041a3b8175
Jahan Chaware <sg550js@gmail.com>
2026-05-07 23:24:38 +0530

    added debug logging

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 )
 
 func main() {
+
+	// add logging to a file for debugging
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Println("fatal:", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
 	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
